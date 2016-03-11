3e4099b52e870b572cee955e3bb45613a141142d
Lisa Lin <lisaylin@outlook.com>
2016-03-11 22:06:06 +0000

    Done with re-former

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# re-former
+The Odin Project - Rails Course - [Project: Bare Metal Forms and Helpers]()
+===========================================================================
+
+This is part of the Forms Project in The Odin Project's Ruby on Rails Curriculum.
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  resources :users, :only => [:new, :create, :edit, :update]
+  
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
